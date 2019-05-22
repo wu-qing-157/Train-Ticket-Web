b8a9e500bf302aa109ab19540d0b801c159b5e5f
wu-qing-157 <40889172+wu-qing-157@users.noreply.github.com>
2019-05-22 17:34:01 +0800

    complete layout for buy ticket

--- nodejs/mdc/order_confirm.jsx
+++ nodejs/mdc/order_confirm.jsx
@@ -1,5 +1,16 @@
 import {MDCSelect} from '@material/select/component'
+import {MDCTextField} from "@material/textfield/component";
+import {MDCTextFieldHelperText} from "@material/textfield/index";
 
 window.order_confirm_init = function () {
     window.kind_select = new MDCSelect(document.getElementById('kind-select'));
+    window.num_text_field = new MDCTextField(document.getElementById('num-text-field'));
+    window.num_helper_text = new MDCTextFieldHelperText(document.getElementById('num-helper-text'));
+    kind_select.listen('MDCSelect:change', () => {
+        document.getElementById('num-helper-text').innerText = '数量不能超过余票（' + window.left_num[kind_select.selectedIndex] + '张）';
+        document.getElementById('num-label').innerText = '数量（余' + window.left_num[kind_select.selectedIndex] + '）张';
+        document.getElementById('num').setAttribute('max', '' + window.left_num[kind_select.selectedIndex]);
+        document.getElementById('num').oninput(null);
+        document.getElementById('num').focus();
+    })
 };
