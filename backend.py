d18af62fe8531644f083df44de902cf5f36741d7
wu-qing-157 <40889172+wu-qing-157@users.noreply.github.com>
2019-05-23 17:28:22 +0800

    remove logger when request from backend

--- backend.py
+++ backend.py
@@ -1,12 +1,9 @@
 import socket, re
 
-from app import app
-
 back_end = ("127.0.0.1", 8081)
 
 
 def get_result(info, buff_size, result_validator):
-    app.logger.info("Request: {}".format(info.split(' ')[0]))
     global sk
     try:
         sk = socket.socket()
