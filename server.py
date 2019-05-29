9dd4fadb40c5f5b59e73c08e34d2275e43e704bb
oscar <oscar@mail.dhc.moe>
2019-05-29 19:51:09 +0800

    improve server.py

--- server.py
+++ server.py
@@ -12,31 +12,29 @@ def format_address(address):
 class MyServer(socketserver.BaseRequestHandler):
 
     def handle(self):
-        print("(Connected) {}".format(format_address(self.client_address)))
-        connection = self.request
-        connection.settimeout(60)
+        global stdout_lock
         try:
-            recv = connection.recv(1024)
-            print("(Receive) {} {}".format(format_address(self.client_address), str(recv, encoding="utf-8")))
-
-            global stdout_lock
             if stdout_lock.acquire():
+                print("(Connected) {}".format(format_address(self.client_address)))
+                connection = self.request
+                connection.settimeout(60)
                 try:
-                    tosend = bytes(input("--- Response? --- >>> "), encoding="utf-8")
-                    connection.sendall(tosend)
-                    print("(Send) {} {}".format(format_address(self.client_address), str(tosend, encoding="utf-8")))
-                except BrokenPipeError:
-                    print("(Error) Client disconnected")
-                finally:
-                    stdout_lock.release()
-            else:
-                print("(Error) Server busy")
-        except socket.timeout:
-            print("(Timeout) {}".format(format_address(self.client_address)))
-        print("(Finish) {}".format(format_address(self.client_address)))
+                    recv = connection.recv(1024)
+                    print("--- Receive   --- <<< {}".format(str(recv, encoding="utf-8")))
+                    try:
+                        tosend = bytes(input("--- Response? --- >>> "), encoding="utf-8")
+                        connection.sendall(tosend)
+                        print("(Send) {} {}".format(format_address(self.client_address), str(tosend, encoding="utf-8")))
+                    except BrokenPipeError:
+                        print("(Error) Client disconnected")
+                except socket.timeout:
+                    print("(Timeout) {}".format(format_address(self.client_address)))
+        finally:
+            stdout_lock.release()
+        print("")
 
 
 if __name__ == "__main__":
     # exe = Popen(['./server'], shell = True, stdin = PIPE, stdout = PIPE)
-    server = socketserver.ThreadingTCPServer(("127.0.0.1", 8081), MyServer)
+    server = socketserver.ThreadingTCPServer(("0.0.0.0", 5009), MyServer)
     server.serve_forever()
